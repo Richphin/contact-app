df83bfd9b8bd145d53dcd9b940dbbac4a5ad7ee1
Richie <richphinsalah6@gmail.com>
2021-06-16 21:06:24 +0000

    added firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDwKEx-p7BRqQjf5MQrXsIDpGBX2GfP6Ag",
+  authDomain: "contact-app-6fef3.firebaseapp.com",
+  projectId: "contact-app-6fef3",
+  storageBucket: "contact-app-6fef3.appspot.com",
+  messagingSenderId: "784803680008",
+  appId: "1:784803680008:web:7136991a45503e509266bd"
+    
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase;
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,15 @@
-import { createStore } from 'redux'
+import { createStore,applyMiddleware, compose } from 'redux'
 import contactReducer from '../reducers/contactReducers'
+import { getFirebase,reduxReactFirebase } from "react-redux-firebase";
+import { getFirestore,reduxFirestore } from "redux-firestore";
+import firebase from "../firebase/config";
+import thunk from 'redux-thunk';
 
  
 
 
- export const store = createStore(contactReducer)
+ export const store = createStore(contactReducer,compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase,getFirestore})),
+    reduxReactFirebase(firebase),
+    reduxFirestore(firebase)
+));
